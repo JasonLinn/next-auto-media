5799513722540057ae4dc041cc47113d05bede56
Jasonlinn <jasonlinn@example.com>
2025-03-16 11:26:28 +0800

    添加 webpack 配置以解決樣式問題

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
